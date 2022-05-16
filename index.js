24ea237a2d80f8b4f571ec13d491640a738fd3ec
Susan Minott <susan_minott@yahoo.com>
2022-05-16 13:26:12 -0400

    commit tree 65001797a909b49d16884039489a378be6fa8195

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
+function toTitleCase(str) {
+  return str.toLowerCase().split(' ').map(function (word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
 const titleCased = () => {
   return tutorials
 }
